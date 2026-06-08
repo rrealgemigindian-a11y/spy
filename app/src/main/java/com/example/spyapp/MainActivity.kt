ff261b6514b6ceb0cb110359c69e866cf76d1fc1
World-H <rrealgemigindian@gmail.com>
2026-06-08 15:46:47 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/spyapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.spyapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.helloTextView)
+        textView.text = "Hello from SpyApp!"
+    }
+}
